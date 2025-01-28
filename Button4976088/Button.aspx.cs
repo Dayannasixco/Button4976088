41847c06b15656fb3597761fa90b255b31266663
DELL <DELL@DAYANNASIXCO>
2025-01-28 13:57:50 -0600

    Agregar archivos de proyecto.

---
+++ Button4976088/Button.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Button4976088
+{
+    public partial class Button : System.Web.UI.Page
+    {
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            lblMessage.ForeColor = System.Drawing.Color.Green;
+            lblMessage.Text = "The email address" + txtEmailAddress.Text + "was successfully saved.";
+        }
+    }
+}
